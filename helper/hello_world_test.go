345bfa5ae27106f8546fb19b08b9cf76e2e19691
widiand <widiand36@gmail.com>
2025-07-06 12:22:08 +0700

    how to handle when fail

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -1,19 +1,46 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestHelloWorldEko(t *testing.T) {
 	result := HelloWorld("Eko")
 	if result != "Hello Eko" {
 		//unit test failed
-		panic("Result is not Hello Eko")
+		t.Fail() //masih lanjut ke print
 	}
+
+	fmt.Println("TestHelloWorldEko done")
 }
 
 func TestHelloWorldKhanndey(t *testing.T) {
 	result := HelloWorld("Khannedy")
 	if result != "Hello Khannedy" {
 		//unit test failed
-		panic("Result is not Hello Khannedy")
+		t.FailNow() //langsung berhenti
 	}
+
+	fmt.Println("TestHelloWorldKhanndey done")
+}
+
+func TestHelloWorldEko1(t *testing.T) {
+	result := HelloWorld("Eko")
+	if result != "Hello Eko" {
+		//unit test failed
+		t.Error("harusnya Hello Eko") //after logging the error will call Fail()
+	}
+
+	fmt.Println("dieksekusi")
+}
+
+func TestHelloWorldEko2(t *testing.T) {
+	result := HelloWorld("Eko")
+	if result != "Hello Eko" {
+		//unit test failed
+		t.Fatal("harusnya Hello Eko") //after logging the error will call FailNow()
+	}
+
+	fmt.Println("tidak dieksekusi")
 }
