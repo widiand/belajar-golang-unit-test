eb0f4feea322405c1f54e2d21d5d32c5ad3d0d3d
widiand <widiand36@gmail.com>
2025-07-06 12:49:17 +0700

    assertion

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -3,7 +3,20 @@ package helper
 import (
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
+
+func TestHelloWorldRequire(t *testing.T) {
+	result := HelloWorld("Eko")
+	require.Equal(t, "Hello Eko", result, "harusnya Hello Eko")//FailNow if fail (not continue)
+}
+
+func TestHelloWOrldAssert(t *testing.T) {
+	result := HelloWorld("Eko")
+	assert.Equal(t, "Hello Eko", result, "harusnya Hello Eko")//Fail if fail (still continnue)
+}
 
 func TestHelloWorldEko(t *testing.T) {
 	result := HelloWorld("Eko")
